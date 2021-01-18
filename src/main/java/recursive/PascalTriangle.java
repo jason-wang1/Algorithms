a1de2126438d7cbd25ebcc2da85a6781fc89ce3c
wangbo <1016765655@qq.com>
2021-01-18 17:50:28 +0800

    add comment

--- src/main/java/recursive/PascalTriangle.java
+++ src/main/java/recursive/PascalTriangle.java
@@ -3,6 +3,20 @@ package recursive;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ *
+ * 示例：
+ * 输入: 5
+ * 输出:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ */
 public class PascalTriangle {
     public static void main(String args[]){
         List<List<Integer>> triangle = generate(5);
